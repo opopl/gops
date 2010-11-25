9cf5cbb3e3b8cd2bee60b35deee7cc89d4e423a9
Oleksandr Poplavskyy <op226@clust.ch.cam.ac.uk>
2010-11-25 19:50:28 +0000

    renamed:    change.sh -> adjust_data.sh 	modified:   adjust_data.sh 	modified:   loop_forces.sh 	modified:   switch_mode.sh

--- adjust_data.sh
+++ adjust_data.sh
@@ -1,11 +1,30 @@
+#!/bin/bash
 
-# adjust the GMIN input file 'data'
+# input file with keywords
+#source f.sh
 
-change.sh pull_start $pull_start
-change.sh pull_end $pull_end
-change.sh nsteps $nsteps
-change.sh force $force
-change.sh temperature $temp
-change.sh radius $radius
-change.sh sys $sys
-echo "COMMENT Changed by O.P. on `date_dm_hm` " >> data.G46
+f=data.G46
+		
+keywords=( "P46" "G46" )
+
+while [ ! -z "$1" ]
+	do 
+		case "$1" in
+	"f" | "force") cat $f | sed "/PULL/ s/[0-9\.DE-]*$/$2/" >& n; mv n $f ;;
+	"t" | "temperature") 
+		awk '/TEMPERATURE/  { sub("^[0-9.]*", par, $2);  }{ print $0 }' par=$2 $f > n ; mv n $f ;;
+	"nsteps")      awk '/STEPS/  { sub("^[0-9ED.]*", par, $2);  }{ print $0 }' par=$2 $f > n; mv n $f ;;
+	"target")      awk '/TARGET/  { sub("^[0-9ED.-]*", par, $2);  }{ print $0 }' par=$2 $f > n; mv n $f ;;
+	"pull_start")  awk '/PULL/  { sub("^[0-9.]*", par, $2);  }{ print $0 }' par=$2 $f > n; mv n $f ;;
+	"pull_end")    awk '/PULL/  { sub("^[0-9.]*", par, $3);  }{ print $0 }' par=$2 $f > n; mv n $f ;;
+	"radius")      awk '/RADIUS/  { sub("^[0-9.]*", par, $2);  }{ print $0 }' par=$2 $f > n; mv n $f ;;
+	"sys")
+       		echo "... source change.sh sys $2"	>> $output_dir/r.log
+		for keyword in "${keywords[@]}"
+			do
+				cat $f | sed "s/^[ ]*$keyword[ ]*/$2 /" > n; mv n $f 
+	      	done
+	;;
+	esac
+	shift
+done
