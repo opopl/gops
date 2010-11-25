b5b2eee5049489cc94ba9d3c302d92c451e309b4
Oleksandr Poplavskyy <op226@clust.ch.cam.ac.uk>
2010-11-25 19:09:14 +0000

    modified:   sb 	modified:   sb_set_pars.sh

--- sb_set_pars.sh
+++ sb_set_pars.sh
@@ -1,45 +1,39 @@
 
-#n1=$(($n%2)) 
-#n2=$(($n/2)) 
+radius=2.0; nrg=1 ; temp=0.03 ; nst=5; queue=s2
+sys="G46"
 
-#case "$n2" in
-	#0) fr=( 0.0001 0.001 ) ;;
-	#1) fr=( 0.001 0.01 ) ;;
-	#2) fr=( 0.01 0.1 ) ;;
-	#3) fr=( 0.1 1.0 ) ;;
-#esac
-
-#case "$n1" in
-          #0) sys="G46" ; od="go$n2" ; queue=s1 ;;
-	#1) sys="P46" ; od="wt$n2" ; queue=s1 ;;
-#esac
-
-#let nf=9
-
-radius=2.0; nrg=1 ; temp=0.03 ; nst=2; queue=s2
-
-fr=( 0.0 0.0001 )
-let nf=100
-
-sys="P46"
-nsteps=10000
+case "$sb_mode" in
+  	1) # {{{
+		n1=$(($n%2)) 
+		n2=$(($n/2)) 
+		
+		case "$n2" in
+			0) fr=( 0.0001 0.001 ) ;;
+			1) fr=( 0.001 0.01 ) ;;
+			2) fr=( 0.01 0.1 ) ;;
+			3) fr=( 0.1 1.0 ) ;;
+		esac
+		
+		case "$n1" in
+			0) sys="G46" ; od="go$n2" ; queue=s1 ;;
+			1) sys="P46" ; od="wt$n2" ; queue=s1 ;;
+		esac
+		
+		let nf=9
+		;;
+	      # }}}	
+	2) fr=( 0.0 0.0001 ) ; nst=4; let nf=100 ; od="out" ;;
+esac
 
 force_min="${fr[0]}"
 force_max="${fr[1]}"
 force_i=$( echo "scale=10; ($force_max-$force_min)/$nf" | bc )
+nsteps=$( echo "10^$nst" | bc )
 
-od="$od"_N_`printf "%1.0e\n" $nsteps`
-od="$od"_f_`printf "%1.1e\n" $force_min`
-od="$od"_`printf "%1.1e\n" $force_max`
-
-od="out"
+#od="$od"_N_`printf "%1.0e\n" $nsteps`
+#od="$od"_f_`printf "%1.1e\n" $force_min`
+#od="$od"_`printf "%1.1e\n" $force_max`
 
 source $shd/define_forces_0.sh 
 
-#case $nst in
-          #3) nsteps=1000000 ;;
-          #2) nsteps=100000 ;;
-          #1) nsteps=10000 ;;
-          #0) nsteps=1000 ;;
-#esac
 
