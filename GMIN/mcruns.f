18ae6775ecc01632cea597777ff1b03745c2a9e3
gates@Gates-PC5 <gates@Gates-PC5>
2011-04-01 20:51:11 +0100

    GMIN - main.F mc.F mcruns.f - MM

--- GMIN/mcruns.f
+++ GMIN/mcruns.f
@@ -1,30 +1,6 @@
-!op226>=================================== 
-!op226> GPL License Info {{{ 
-C   GMIN: A program for finding global minima
-C   Copyright (C) 1999-2006 David J. Wales
-C   This file is part of GMIN.
-C
-C   GMIN is free software; you can redistribute it and/or modify
-C   it under the terms of the GNU General Public License as published by
-C   the Free Software Foundation; either version 2 of the License, or
-C   (at your option) any later version.
-C
-C   GMIN is distributed in the hope that it will be useful,
-C   but WITHOUT ANY WARRANTY; without even the implied warranty of
-C   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-C   GNU General Public License for more details.
-C
-C   You should have received a copy of the GNU General Public License
-C   along with this program; if not, write to the Free Software
-C   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-C
-!op226>}}} 
-!op226>=================================== 
-!> \brief This subroutine ...
-!> \name MCRUNS
-!> \param SCREENC  
-!op226>=================================== 
+
       SUBROUTINE MCRUNS(SCREENC)
+
       USE COMMONS
       IMPLICIT NONE
       LOGICAL LOPEN
@@ -33,6 +9,7 @@ C
       INTEGER J1
 
       CALL MC(MCSTEPS(1),TFAC(1),SCREENC)
+
       CALL FINALQ
       CALL FINALIO
 
