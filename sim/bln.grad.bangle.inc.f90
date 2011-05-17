22599908a754f2292b6be24703b97d145d56fb09
op <op>
2011-05-17 19:48:23 +0100

    bln wip

--- sim/bln.grad.bangle.inc.f90
+++ sim/bln.grad.bangle.inc.f90
@@ -1,19 +1,175 @@
 
-! bond angle forces: particle 1
-! particles 1, 2, N-1, and N are done outside of the loop
-! {{{
+! sim part {{{
+!! bond angle forces: particle 1
+!! particles 1, 2, N-1, and N are done outside of the loop
+!! {{{
+        !i = 1
+        !den = sin(ang(i+1,1))*sqrt(dpd(i+1,1)*dpd(i,1))
+        !rnum = rk_theta*(ang(i+1,1) - theta_0)
+        !fba(i,1:3)=-rnum*((dpd(i,2)/dpd(i,1))*dr(i,i+1,1:3)-dr(i+1,i+2,1:3))/den
+!! }}}
+     !! particle 2
+!! {{{
+
+        !i = 2
+        !den = sin(bond_angle(i))
+     !1        *dsqrt(dot_prod(i,1)*dot_prod(i-1,1))
+        !den1 = sin(bond_angle(i+1))*dsqrt(dot_prod(i+1,1)
+     !1         *dot_prod(i,1))
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*xr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *xr(i-1,i) + xr(i,i+1) - xr(i-1,i))/den
+
+        !a2 = -rk_theta*(bond_angle(i+1) - theta_0)*((dot_prod(i,2)/
+     !1        dot_prod(i,1))*xr(i,i+1) - xr(i+1,i+2))/den1
+
+        !fba_x(i) = a1 + a2 
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*yr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *yr(i-1,i) + yr(i,i+1) - yr(i-1,i))/den
+
+        !a2 = -rk_theta*(bond_angle(i+1) - theta_0)*((dot_prod(i,2)/
+     !1        dot_prod(i,1))*yr(i,i+1) - yr(i+1,i+2))/den1
+
+        !fba_y(i) = a1 + a2 
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*zr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *zr(i-1,i) + zr(i,i+1) - zr(i-1,i))/den
+
+        !a2 = -rk_theta*(bond_angle(i+1) - theta_0)*((dot_prod(i,2)/
+     !1        dot_prod(i,1))*zr(i,i+1) - zr(i+1,i+2))/den1
+
+        !fba_z(i) = a1 + a2 
+
+!! }}}
+!! particles 3 thru n-2 !{{{
+
+        !do i = 3, n-2
+
+        !den = sin(bond_angle(i))*
+     !1              dsqrt(dot_prod(i,1)*dot_prod(i-1,1))
+        !den1 = sin(bond_angle(i+1))*
+     !1               dsqrt(dot_prod(i+1,1)*dot_prod(i,1))
+        !den2 = sin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
+     !1         *dot_prod(i-1,1))
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*xr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *xr(i-1,i) + xr(i,i+1) - xr(i-1,i))/den
+
+        !a2 = -rk_theta*(bond_angle(i+1) - theta_0)*((dot_prod(i,2)/
+     !1        dot_prod(i,1))*xr(i,i+1) - xr(i+1,i+2))/den1
+
+        !a3 = rk_theta*(bond_angle(i-1) - theta_0)*((dot_prod(i-2,2)/
+     !1        dot_prod(i-1,1))*xr(i-1,i) - xr(i-2,i-1))/den2
+
+        !fba_x(i) = a1 + a2 + a3 
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*yr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *yr(i-1,i) + yr(i,i+1) - yr(i-1,i))/den
+
+        !a2 = -rk_theta*(bond_angle(i+1) - theta_0)*((dot_prod(i,2)/
+     !1        dot_prod(i,1))*yr(i,i+1) - yr(i+1,i+2))/den1
+
+        !a3 = rk_theta*(bond_angle(i-1) - theta_0)*((dot_prod(i-2,2)/
+     !1        dot_prod(i-1,1))*yr(i-1,i) - yr(i-2,i-1))/den2
+
+        !fba_y(i) = a1 + a2 + a3 
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*zr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *zr(i-1,i) + zr(i,i+1) - zr(i-1,i))/den
+
+        !a2 = -rk_theta*(bond_angle(i+1) - theta_0)*((dot_prod(i,2)/
+     !1        dot_prod(i,1))*zr(i,i+1) - zr(i+1,i+2))/den1
+
+        !a3 = rk_theta*(bond_angle(i-1) - theta_0)*((dot_prod(i-2,2)/
+     !1        dot_prod(i-1,1))*zr(i-1,i) - zr(i-2,i-1))/den2
+
+        !fba_z(i) = a1 + a2 + a3 
+
+        !enddo
+!!}}}
+!! particle n-1 {{{
+        !i = n-1
+        !den = sin(bond_angle(i))*
+     !1              dsqrt(dot_prod(i,1)*dot_prod(i-1,1))
+        !den1 = sin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
+     !1         *dot_prod(i-1,1))
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*xr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *xr(i-1,i) + xr(i,i+1) - xr(i-1,i))/den
+
+        !a2 = rk_theta*(bond_angle(i-1) - theta_0)*((dot_prod(i-2,2)/
+     !1        dot_prod(i-1,1))*xr(i-1,i) - xr(i-2,i-1))/den1
+
+        !fba_x(i) = a1 + a2
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*yr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *yr(i-1,i) + yr(i,i+1) - yr(i-1,i))/den
+
+        !a2 = rk_theta*(bond_angle(i-1) - theta_0)*((dot_prod(i-2,2)/
+     !1        dot_prod(i-1,1))*yr(i-1,i) - yr(i-2,i-1))/den1
+
+        !fba_y(i) = a1 + a2
+
+        !a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
+     !1  dot_prod(i,1))*zr(i,i+1) - (dot_prod(i-1,2)/dot_prod(i-1,1))
+     !1        *zr(i-1,i) + zr(i,i+1) - zr(i-1,i))/den
+
+        !a2 = rk_theta*(bond_angle(i-1) - theta_0)*((dot_prod(i-2,2)/
+     !1        dot_prod(i-1,1))*zr(i-1,i) - zr(i-2,i-1))/den1
+
+        !fba_z(i) = a1 + a2
+!!}}}
+!! particle n!{{{
+
+        !i = n
+        !den = sin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
+     !1        *dot_prod(i-1,1))
+
+        !fba_x(i) = rk_theta*(bond_angle(i-1) - theta_0)*
+     !1        ((dot_prod(i-2,2)/dot_prod(i-1,1))*xr(i-1,i) 
+     !1        - xr(i-2,i-1))/den
+
+        !fba_y(i) = rk_theta*(bond_angle(i-1) - theta_0)*
+     !1        ((dot_prod(i-2,2)/dot_prod(i-1,1))*yr(i-1,i) 
+     !1        - yr(i-2,i-1))/den
+
+        !fba_z(i) = rk_theta*(bond_angle(i-1) - theta_0)*
+     !1        ((dot_prod(i-2,2)/dot_prod(i-1,1))*zr(i-1,i) 
+     !1        - zr(i-2,i-1))/den!}}}
+!! }}}
+
+C bond angle forces  particle 1
+C particles 1,2,n-1, and n done outside of the loop
+C {{{
         i = 1
-        den = sin(ang(i+1,1))*sqrt(dpd(i+1,1)*dpd(i,1))
-        rnum = rk_theta*(ang(i+1,1) - theta_0)
-        fba(i,1:3)=-rnum*((dpd(i,2)/dpd(i,1))*dr(i,i+1,1:3)-dr(i+1,i+2,1:3))/den
-! }}}
-     ! particle 2
-! {{{
+        den = dsin(bond_angle(i+1))
+     1        *dsqrt(dot_prod(i+1,1)*dot_prod(i,1))
+        rnum = rk_theta*(bond_angle(i+1) - theta_0)
+
+        fba_x(i) = -rnum*((dot_prod(i,2)/dot_prod(i,1))*xr(i,i+1) -
+     1        xr(i+1,i+2))/den
+
+        fba_y(i) = -rnum*((dot_prod(i,2)/dot_prod(i,1))*yr(i,i+1) -
+     1        yr(i+1,i+2))/den
+
+        fba_z(i) = -rnum*((dot_prod(i,2)/dot_prod(i,1))*zr(i,i+1) -
+     1        zr(i+1,i+2))/den
+
+C particle 2
 
         i = 2
-        den = sin(bond_angle(i))
+        den = dsin(bond_angle(i))
      1        *dsqrt(dot_prod(i,1)*dot_prod(i-1,1))
-        den1 = sin(bond_angle(i+1))*dsqrt(dot_prod(i+1,1)
+        den1 = dsin(bond_angle(i+1))*dsqrt(dot_prod(i+1,1)
      1         *dot_prod(i,1))
 
         a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
@@ -43,16 +199,15 @@
 
         fba_z(i) = a1 + a2 
 
-! }}}
-! particles 3 thru n-2 !{{{
+C particles 3 thru n-2 
 
         do i = 3, n-2
 
-        den = sin(bond_angle(i))*
+        den = dsin(bond_angle(i))*
      1              dsqrt(dot_prod(i,1)*dot_prod(i-1,1))
-        den1 = sin(bond_angle(i+1))*
+        den1 = dsin(bond_angle(i+1))*
      1               dsqrt(dot_prod(i+1,1)*dot_prod(i,1))
-        den2 = sin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
+        den2 = dsin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
      1         *dot_prod(i-1,1))
 
         a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
@@ -92,12 +247,13 @@
         fba_z(i) = a1 + a2 + a3 
 
         enddo
-!}}}
-! particle n-1 {{{
+
+C particle n-1 
+
         i = n-1
-        den = sin(bond_angle(i))*
+        den = dsin(bond_angle(i))*
      1              dsqrt(dot_prod(i,1)*dot_prod(i-1,1))
-        den1 = sin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
+        den1 = dsin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
      1         *dot_prod(i-1,1))
 
         a1 = -rk_theta*(bond_angle(i) - theta_0)*( (dot_prod(i-1,2)/
@@ -126,11 +282,11 @@
      1        dot_prod(i-1,1))*zr(i-1,i) - zr(i-2,i-1))/den1
 
         fba_z(i) = a1 + a2
-!}}}
-! particle n!{{{
+
+C particle n
 
         i = n
-        den = sin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
+        den = dsin(bond_angle(i-1))*dsqrt(dot_prod(i-2,1)
      1        *dot_prod(i-1,1))
 
         fba_x(i) = rk_theta*(bond_angle(i-1) - theta_0)*
@@ -143,6 +299,6 @@
 
         fba_z(i) = rk_theta*(bond_angle(i-1) - theta_0)*
      1        ((dot_prod(i-2,2)/dot_prod(i-1,1))*zr(i-1,i) 
-     1        - zr(i-2,i-1))/den!}}}
-! }}}
+     1        - zr(i-2,i-1))/den
+C }}}
 
--- sim/bln.grad.nbond.inc.f90
+++ sim/bln.grad.nbond.inc.f90
@@ -4,10 +4,14 @@
         DO I = 1, N-2
           DO J = I+2, N
   
-            RAD(1)=LEN_DR(I,J) ; RAD(7)=RAD(1)**7 ; RAD(14)=RAD(7)**2 ; RAD(8)=RAD(7)*RAD(1)
+            RAD(1)=LEN_DR(I,J) ; 
+            RAD(2)=RAD(1)**2 ; 
+            RAD(7)=RAD(1)**7 ; 
+            RAD(14)=RAD(7)**2 ; 
+            RAD(8)=RAD(7)*RAD(1)
         
-            DF = 2.0*AB(I,J,1)*S(12)/RAD(14) + AB(I,J,2)*S(6)/RAD(8)
-            DF=-24.0*DF
+            DF = 2.0*AB(I,J,1)*S(12)/RAD(12) + AB(I,J,2)*S(6)/RAD(6)
+            DF=-24.0*DF/RAD(2)
 
             FRR(1:3) = DF*DR(I,J,1:3) 
             FNB(I,1:3) = FRR(1:3) + FNB(I,1:3)
