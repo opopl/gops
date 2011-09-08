940f3c11f642cf1f687c17cf57c7ac3b171c9528
op <op>
2011-09-08 21:11:33 +0100

    M kw.f90 ( added code for 'data' file )

--- sim/kw.f90
+++ sim/kw.f90
@@ -29,7 +29,14 @@ CONTAINS
       SELECTCASE(PROG)
         CASE(1) ! GMIN {{{
 
-      CALL OPENF(DATA_FH,"<","data")
+      CALL INQF('data',YESNO)
+
+      IF (YESNO) THEN 
+         CALL OPENF(DATA_FH,"<","data")
+      ELSE
+         PRINT '(A)','ERROR - no data file'
+         STOP
+      ENDIF
 
 ! ios<0 end of file;
 ! ios=0 
